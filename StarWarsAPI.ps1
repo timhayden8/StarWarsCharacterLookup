08e207fd9099a5b619a989aacf1779b7d950cf68
timhayden8 <119264210+timhayden8@users.noreply.github.com>
2023-03-27 23:15:55 -0600

    Update StarWarsAPI.ps1

--- StarWarsAPI.ps1
+++ StarWarsAPI.ps1
@@ -1,8 +1,19 @@
-#this function uses the swapi to look up a particular star wars character and list all relevant information about them.
+#this function uses the swapi to look up a particular star wars character. It then grabs its homeworld using another section of the star wars API and finally connects to a discord webhook to comment what character is from where.
 function starwarscharacterlookup
 {
     param ([string]$character)
     $Characters = invoke-restmethod https://swapi.dev/api/people
-    $characters.results | where-object {$_.name -like "$character"} 
+    $lookup = ($characters.results | where-object {$_.name -like "*$character*"}) 
+    $hw = (invoke-restmethod $lookup.homeworld)
+    $hw=$hw.name
+    $lookup=$lookup.name
+    $content="$lookup is from $hw"
+    $payload = [pscustomobject]@{
+        content = $content
+    }
+    #discord webhook
+    $Hook="https://discord.com/api/webhooks/1090132828894806126/gXsU36xyPMhyaid8htTLbh7F3NM4JsnQMNTUs4_dtZJ4nEcrjUEK3UXEiRVkzTnOmOy9"
+    invoke-restmethod $hook -method Post -body ($payload | convertto-json) -ContentType 'application/json'
+
 }
 starwarscharacterlookup -character "Obi-Wan"
